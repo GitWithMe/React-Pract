2e3b8884b98e67aaa205139d8505bd62070eed38
Namit Dhupar <33388782+GitWithMe@users.noreply.github.com>
2019-12-17 17:06:53 +0530

    forth commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -5,37 +5,33 @@ import Prsn from './Person/Person.js';
 class App extends Component {
   state = { //Used to make the props more dynamic
     person: [
-    {name:"Namit", age: 23},
-    {name:"Panda", age: 18},
-    {name:"Tarun", age: 50},
-    {name:"Meenu", age: 48},
-    {name:"Cherry", age: 3}
+    {id:"1", name:"Namit", age: 23},
+    {id:"2", name:"Krrish", age: 18},
+    {id:"3", name:"Tarun", age: 50},
+    {id:"4", name:"Meenu", age: 48},
+    {id:"5", name:"Cherry", age: 3}
     ],
     showPerson: false // state for toggling an event
   }
 
-  myClickButton = (newName) =>{ //Used to bind the states like in onClick event 
-    this.setState({ //changes the state defined above
-     person: [
-    {name:newName , age: 23},
-    {name:"Krrish", age: 17},
-    {name:"Tarun", age: 50},
-    {name:"Meenu", age: 48},
-    {name:"Cherry", age: 3}
-    ] 
-    })
+  deletePerson = (personIndex) =>{
+   const person = [...this.state.person]; //Copying the person array from the state object
+   person.splice(personIndex,1); //
+   this.setState({person : person});
   }
 
-   myNameChange = (event) =>{ //Event Target Value are for input field 
+   myNameChange = (event, id) =>{ //Event Target Value are for input field
+    const personIndex = this.state.person.findIndex((pers) => { //If this returns true then only it stores the value in the variable
+      return pers.id === id; //makes use of the id in the person's array in the state object
+    });
+    const myperson = {...this.state.person[personIndex]}; //copies the original array and finds the required
+    myperson.name = event.target.value;
+
+    //For updating the state we need to have a const variable name of same array
+    const person = [...this.state.person];
+    person[personIndex] = myperson;
     this.setState({ //changes the state defined above
-     person: [
-    {name:event.target.value, age: 23},
-    {name:"Krrish", age: 17},
-    {name:"Tarun", age: 50},
-    {name:"Meenu", age: 48},
-    {name:"Cherry", age: 3}
-    ] 
-    })
+     person: person});
   }
 
   togglePerson = () =>{
@@ -60,30 +56,22 @@ class App extends Component {
     if(this.state.showPerson){  //if true
       person = (
        <div>
-        <Prsn
-        name = {this.state.person[0].name} 
-        age = {this.state.person[0].age}
-        click = {this.myClickButton.bind(this, "Ya Yeet")}
-        changed = {this.myNameChange}></Prsn> 
-        <Prsn 
-        name = {this.state.person[1].name} 
-        age = {this.state.person[1].age}></Prsn>
-        <Prsn 
-        name = {this.state.person[2].name} 
-        age = {this.state.person[2].age}></Prsn>
-        <Prsn
-        name = {this.state.person[3].name} 
-        age = {this.state.person[3].age}></Prsn>
-        <Prsn 
-        name = {this.state.person[4].name} 
-        age = {this.state.person[4].age}>I am Dogu</Prsn>
-        </div>
+       {this.state.person.map((person, index) => { //creating a list by dynamically mapping each element of person array along with their index into the jsx code
+          return <Prsn style = {style}
+          name = {person.name}
+          age = {person.age}
+          key = {person.id /*Identify the array elements uniquely*/} 
+          click = {() => this.deletePerson(index) /*delete the particular index clicked on*/}
+          changed = {(event) => this.myNameChange(event, person.id)}
+          />
+       })}
+       </div>
         );
    }
       //Calling the person variable of the render method in the second button
     return (
       <div className="App">
-      <button style={style} onClick = {this.myClickButton.bind(this, "Baby Yoda")}>Change Name</button>
+      <button style={style}>Change Name</button>
       <h1>React Practice!</h1>
       <button style={style} onClick = {this.togglePerson}>Toggle People</button>
       {person}
